bd16d38b25950619d0a31b7b53fcdad2b63b5311
Sabhya Mittal <74859174+sabhyamittal@users.noreply.github.com>
2024-04-27 12:14:22 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (50.17%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toHex(int num) {
+        unsigned int temp=num;
+        if(num==0)return "0";
+        string s;
+        char arr[]={"0123456789abcdef"};
+        while(temp)
+        {
+            s+=arr[temp%16];
+            temp=temp/16;
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
